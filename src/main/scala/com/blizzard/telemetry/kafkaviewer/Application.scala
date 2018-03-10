568e7fc9683322cdd9e537fe755b9cf9b0b2f038
Stephen Parente <webmakersteve@gmail.com>
2018-03-10 14:59:26 -0800

    Fixed formatting

--- src/main/scala/com/blizzard/telemetry/kafkaviewer/Application.scala
+++ src/main/scala/com/blizzard/telemetry/kafkaviewer/Application.scala
@@ -4,85 +4,87 @@ import java.awt.image.BufferedImage
 import javax.swing.{ImageIcon, JFrame, JLabel}
 import java.awt.{BorderLayout, Dimension}
 
-object Application {
-	def main(args: Array[String]) : Unit = {
-		val frame = openWindow
+object Application extends App {
+	lazy val frame = openWindow
 
-		val consumer = new Consumer(List("test"))
+	val consumer = new Consumer(List("test"))
 
-		consumer.consume((bytes: Array[Byte]) => {
-			val side = Math.floor(Math.sqrt(bytes.length)).asInstanceOf[Int]
+	consumer.consume((bytes: Array[Byte]) => {
+		val side = Math.floor(Math.sqrt(bytes.length)).asInstanceOf[Int]
 
-			//image dimension
-			val width = side
-			val height = side
-			//create buffered image object img
-			val img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		//image dimension
+		val width = side
+		val height = side
+		//create buffered image object img
+		val img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
-			val totalBytes = bytes.size
-			var counter = 0
+		val totalBytes = bytes.size
+		var counter = 0
 
-			for(y <- 0 to height - 1) {
-			  for(x <- 0 to width - 1) {
-
-					def getNextByte : Int = {
-						if (counter >= totalBytes) {
-							0
-						} else {
-							var byteAtIndex = bytes(counter).toInt
-              if (byteAtIndex < 0) {
-                byteAtIndex = 128 + Math.abs(byteAtIndex)
-              }
-							counter+=1
-							return byteAtIndex
-						}
-					}
-
-					//val a : Int = getNextByte // Alpha
-					//val r : Int = getNextByte //red
-					//val g : Int = getNextByte //green
-					//val b : Int = getNextByte //blue
-
-					//val p : Int = (a<<24) | (r<<16) | (g<<8) | b;
-          val pixel = 255 << 24 | getNextByte << 16
-					img.setRGB(x, y, pixel)
-			  }
+		def getNextByte : Int = {
+			if (counter >= totalBytes) {
+				0
+			} else {
+				var byteAtIndex = bytes(counter).toInt
+				if (byteAtIndex < 0) {
+					byteAtIndex = 128 + Math.abs(byteAtIndex)
+				}
+				counter+=1
+				return byteAtIndex
 			}
+		}
 
-      System.out.println(s"Painting ${bytes.length} bytes...")
+		for(y <- 0 to height - 1) {
+			for(x <- 0 to width - 1) {
 
-			paintWindow(frame, img)
-		})
+				//val a : Int = getNextByte // Alpha
+				//val r : Int = getNextByte //red
+				//val g : Int = getNextByte //green
+				//val b : Int = getNextByte //blue
 
-	}
+				//val p : Int = (a<<24) | (r<<16) | (g<<8) | b;
+				val pixel = 255 << 24 | getNextByte << 16
+				img.setRGB(x, y, pixel)
+			}
+		}
 
-	def paintWindow(frame : JFrame, image: BufferedImage) : Unit = {
-		//3. Create components and put them in the frame.
+		System.out.println(s"Painting ${bytes.length} bytes...")
+
+		paintWindow(frame, img)
+	})
+
+	sys.ShutdownHookThread {
+		System.out.println("Got shutdown hook. Shutting down.")
+    consumer.close()
+  }
+
+  def paintWindow(frame: JFrame, image: BufferedImage) : Unit = {
+  	//3. Create components and put them in the frame.
 
     val scaledImage = image.getScaledInstance(100, 100, 0)
     val imageIcon = new ImageIcon(scaledImage)
     val label = new JLabel(imageIcon)
 
-		frame.getContentPane().removeAll()
-		frame.getContentPane().add(label, BorderLayout.CENTER)
-		frame.pack()
-		frame.getContentPane().repaint()
-	}
+	  frame.getContentPane().removeAll()
+	  frame.getContentPane().add(label, BorderLayout.CENTER)
+	  frame.pack()
+	  frame.getContentPane().repaint()
+  }
 
-	def openWindow : JFrame = {
-		//1. Create the frame.
-		val frame = new JFrame("FrameDemo");
+  private def openWindow: JFrame = {
+	  //1. Create the frame.
+	  val frame = new JFrame("KafkaViewer");
 
-		//2. Optional: What happens when the frame closes?
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+	  //2. Optional: What happens when the frame closes?
+	  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
 
-		//4. Size the frame.
+	  //4. Size the frame.
     frame.setMinimumSize(new Dimension(100, 100))
     frame.pack()
 
-		//5. Show it.
-		frame.setVisible(true)
+	  //5. Show it.
+	  frame.setVisible(true)
 
-		return frame
-	}
+	  return frame
+  }
 }
--- src/main/scala/com/blizzard/telemetry/kafkaviewer/Consumer.scala
+++ src/main/scala/com/blizzard/telemetry/kafkaviewer/Consumer.scala
@@ -2,30 +2,40 @@ package com.blizzard.telemetry.kafkaviewer
 
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import java.util.Properties
-import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+import java.util.concurrent.atomic.AtomicBoolean
 
 class Consumer(topics: List[String]) {
 
-	val config = new Properties();
-	config.put("bootstrap.servers", "localhost:9092");
-	config.put("group.id", "test");
-	config.put("auto.offset.reset", "earliest")
-	config.put("enable.auto.commit", "true");
-	config.put("auto.commit.interval.ms", "1000");
-	config.put("session.timeout.ms", "30000");
-	config.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-	config.put("value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
+  private val config = new Properties();
+  config.put("bootstrap.servers", "localhost:9092");
+  config.put("group.id", "test");
+  config.put("auto.offset.reset", "earliest")
+  config.put("enable.auto.commit", "true");
+  config.put("auto.commit.interval.ms", "1000");
+  config.put("session.timeout.ms", "30000");
+  config.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+  config.put("value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
 
-	val consumer = new KafkaConsumer[String, Array[Byte]](config);
-	consumer.subscribe(topics);
+  private val consumer = new KafkaConsumer[String, Array[Byte]](config);
+  consumer.subscribe(topics.asJava);
 
-	def consume(callback: (Array[Byte]) => Unit) : Unit = {
-		while (true) {
-			val records = consumer.poll(100);
-			for (record <- records) {
-				callback(record.value())
-			}
-		}
-	}
+  private var isRunning: AtomicBoolean = new AtomicBoolean(true)
+
+  def consume(callback: (Array[Byte]) => Unit) : Unit = {
+    while (isRunning.get()) {
+      val records = consumer.poll(100);
+      for (record <- records.asScala) {
+        callback(record.value())
+      }
+    }
+  }
+
+  def close(): Unit = {
+    if (isRunning.compareAndSet(true, false)) {
+			consumer.wakeup()
+      consumer.close()
+    }
+  }
 
 }
